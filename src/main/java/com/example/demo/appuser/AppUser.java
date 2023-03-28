dd4aac279821b9b2b2ccab46fed605ba14b50e85
fmintar1 <fmintar1@gmail.com>
2023-03-28 11:41:58 -0400

    added appuser, registration, and security directories

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -7,16 +7,63 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter //lombok
+@Setter //lombok
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity //table in database
 public class AppUser implements UserDetails{
 
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String password;
     private String username;
     private String email;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+
+    
+
+    public AppUser(
+        String name, 
+        String password, 
+        String username, 
+        String email, 
+        AppUserRole appUserRole, 
+        Boolean locked,
+        Boolean enabled) {
+            this.name = name;
+            this.password = password;
+            this.username = username;
+            this.email = email;
+            this.appUserRole = appUserRole;
+            this.locked = locked;
+            this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
